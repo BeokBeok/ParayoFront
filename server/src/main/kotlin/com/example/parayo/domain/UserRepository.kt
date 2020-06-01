9cee85b775d31be70306d8980dd0178d762608c0
BeokBeok <kekemusa37@nate.com>
2020-06-01 22:28:11 +0900

    [추가] UserRepository

---
+++ server/src/main/kotlin/com/example/parayo/domain/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.example.parayo.domain
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long>

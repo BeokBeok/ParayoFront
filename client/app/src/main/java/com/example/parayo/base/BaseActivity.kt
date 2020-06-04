e0fcf183eff746c74d177c64adf469ea29a1a770
BeokBeok <kekemusa37@nate.com>
2020-06-04 21:47:52 +0900

    [추가] BaseActivity

---
+++ client/app/src/main/java/com/example/parayo/base/BaseActivity.kt
@@ -0,0 +1,22 @@
+package com.example.parayo.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<VDB : ViewDataBinding>(
+    @LayoutRes private val layoutId: Int
+) : AppCompatActivity() {
+
+    protected lateinit var binding: VDB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView<VDB>(this, layoutId).apply {
+            lifecycleOwner = this@BaseActivity
+        }
+    }
+}

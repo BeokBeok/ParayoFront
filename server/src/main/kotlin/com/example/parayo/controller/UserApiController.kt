231709564792a189eb59e18bba6855d60da78f5e
BeokBeok <kekemusa37@nate.com>
2020-06-08 22:40:38 +0900

    [추가] 로그인 관련 API

--- server/src/main/kotlin/com/example/parayo/controller/UserApiController.kt
+++ server/src/main/kotlin/com/example/parayo/controller/UserApiController.kt
@@ -1,6 +1,8 @@
 package com.example.parayo.controller
 
 import com.example.parayo.common.ApiResponse
+import com.example.parayo.domain.auth.SignInRequest
+import com.example.parayo.domain.auth.SignInService
 import com.example.parayo.domain.auth.SignUpRequest
 import com.example.parayo.domain.auth.SignUpService
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,10 +14,15 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1")
 class UserApiController @Autowired constructor(
-    private val signUpService: SignUpService
+    private val signUpService: SignUpService,
+    private val signInService: SignInService
 ) {
 
     @PostMapping("/users")
     fun signUp(@RequestBody signUpRequest: SignUpRequest) =
         ApiResponse.ok(signUpService.signUp(signUpRequest))
+
+    @PostMapping("/signin")
+    fun signIn(@RequestBody signInRequest: SignInRequest) =
+        ApiResponse.ok(signInService.signIn(signInRequest))
 }

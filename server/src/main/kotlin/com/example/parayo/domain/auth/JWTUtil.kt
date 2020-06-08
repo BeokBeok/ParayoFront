072f01138bf97834ebddfef19e545d173e44fe8e
BeokBeok <kekemusa37@nate.com>
2020-06-08 22:22:49 +0900

    [추가] jwt 리프레시 토큰 생성 함수

--- server/src/main/kotlin/com/example/parayo/domain/auth/JWTUtil.kt
+++ server/src/main/kotlin/com/example/parayo/domain/auth/JWTUtil.kt
@@ -9,10 +9,15 @@ object JWTUtil {
     private const val ISSUER = "Parayo"
     private const val SUBJECT = "Auth"
     private const val EXPIRE_TIME = 60L * 60 * 2 * 1_000 // 2 hour
-    private const val SECRET = "beok"
+    private const val REFRESH_EXPIRE_TIME = 60L * 60 * 24 * 30 * 1_000 // 30 day
+
+    private const val SECRET = "access"
+    private val algorithm = Algorithm.HMAC256(SECRET)
+
     private const val EMAIL = "email"
 
-    private val algorithm = Algorithm.HMAC256(SECRET)
+    private const val REFRESH_SECRET = "refresh"
+    private val refreshAlgorith = Algorithm.HMAC256(REFRESH_SECRET)
 
     fun createToken(email: String): String {
         val createdDate = Date()
@@ -23,5 +28,16 @@ object JWTUtil {
             .withExpiresAt(Date(createdDate.time + EXPIRE_TIME))
             .withClaim(EMAIL, email)
             .sign(algorithm)
+    }
+
+    fun createRefreshToken(email: String): String {
+        val createdDate = Date()
+        return JWT.create()
+            .withIssuer(ISSUER)
+            .withSubject(SUBJECT)
+            .withIssuedAt(createdDate)
+            .withExpiresAt(Date(createdDate.time + REFRESH_EXPIRE_TIME))
+            .withClaim(EMAIL, email)
+            .sign(refreshAlgorith)
     }
 }

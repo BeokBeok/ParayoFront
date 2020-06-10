42f19937c3002e0e3c5a09495cbaec6ce3b937c6
BeokBeok <kekemusa37@nate.com>
2020-06-10 21:09:43 +0900

    [추가] MainActivity and MainViewModel

---
+++ client/app/src/main/java/com/example/parayo/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.parayo.main
+
+import androidx.activity.viewModels
+import com.example.parayo.R
+import com.example.parayo.base.BaseActivity
+import com.example.parayo.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    layoutId = R.layout.activity_main
+) {
+
+    private val viewModel by viewModels<MainViewModel>()
+
+}
---
+++ client/app/src/main/java/com/example/parayo/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.parayo.main
+
+import androidx.hilt.lifecycle.ViewModelInject
+import com.example.parayo.base.BaseViewModel
+
+class MainViewModel @ViewModelInject constructor() : BaseViewModel()

71cf451f00966e2737e83ecde73c627b309c1fe5
BeokBeok <kekemusa37@nate.com>
2020-06-10 22:08:21 +0900

    [추가] Drawer open close event

--- client/app/src/main/java/com/example/parayo/main/MainActivity.kt
+++ client/app/src/main/java/com/example/parayo/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.parayo.main
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.parayo.R
 import com.example.parayo.base.BaseActivity
 import com.example.parayo.databinding.ActivityMainBinding
@@ -18,6 +19,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupToolbar()
+        setupDrawer()
     }
 
     override fun setupObserver() = Unit
@@ -28,5 +30,17 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
                 .setIcon(R.drawable.ic_baseline_search_24)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
         }
+    }
+
+    private fun setupDrawer() {
+        val drawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.dlMainDrawer,
+            binding.tbMainToolbar,
+            R.string.open_drawer_description,
+            R.string.close_drawer_description
+        )
+        binding.dlMainDrawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
     }
 }

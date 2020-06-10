635a3d08c9f45888596e820615c95b9875e487ef
BeokBeok <kekemusa37@nate.com>
2020-06-10 21:51:52 +0900

    [추가] Toolbar search menu

--- client/app/src/main/java/com/example/parayo/main/MainActivity.kt
+++ client/app/src/main/java/com/example/parayo/main/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.parayo.main
 
+import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.viewModels
 import com.example.parayo.R
 import com.example.parayo.base.BaseActivity
@@ -13,5 +15,18 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
 
     private val viewModel by viewModels<MainViewModel>()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupToolbar()
+    }
+
     override fun setupObserver() = Unit
+
+    private fun setupToolbar() {
+        binding.tbMainToolbar.run {
+            menu.add("Search")
+                .setIcon(R.drawable.ic_baseline_search_24)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        }
+    }
 }

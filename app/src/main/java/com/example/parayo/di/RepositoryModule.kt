672ca93854c5ad92a71a112e9aa3f9e3ff938f27
BeokBeok <kekemusa37@nate.com>
2020-06-28 14:50:57 +0900

    [제거] 불필요 Annotation

--- app/src/main/java/com/example/parayo/di/RepositoryModule.kt
+++ app/src/main/java/com/example/parayo/di/RepositoryModule.kt
@@ -7,14 +7,12 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
-import dagger.hilt.android.scopes.ActivityRetainedScoped
 
 @Module
 @InstallIn(ActivityRetainedComponent::class)
 class RepositoryModule {
 
     @Provides
-    @ActivityRetainedScoped
     fun provideAuthRepository(authService: AuthService): AuthDataSource =
         AuthRepository(authService)
 }

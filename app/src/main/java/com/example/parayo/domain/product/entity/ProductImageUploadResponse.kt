09bea0bed6cac18c616161970c3aecc40b19e1f8
BeokBeok <kekemusa37@nate.com>
2020-07-03 21:02:56 +0900

    [추가] ProductImageUploadResponse 에 SerializedName annotation

--- app/src/main/java/com/example/parayo/domain/product/entity/ProductImageUploadResponse.kt
+++ app/src/main/java/com/example/parayo/domain/product/entity/ProductImageUploadResponse.kt
@@ -1,6 +1,11 @@
 package com.example.parayo.domain.product.entity
 
+import com.google.gson.annotations.SerializedName
+
 class ProductImageUploadResponse(
+    @SerializedName("productImageId")
     val productImageId: Long,
+
+    @SerializedName("filePath")
     val filePath: String
 )

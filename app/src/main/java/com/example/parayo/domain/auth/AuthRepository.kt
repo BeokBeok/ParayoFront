794b1fd5797512cf0e1499240723498f1ee9f409
BeokBeok <kekemusa37@nate.com>
2020-07-13 23:27:44 +0900

    [적용] ioDispatcher
    - 레트로핏에 suspend 를 붙이면 기본으로 ioDispatcher 에서 돈다고 했는데 거짓말이였음

--- app/src/main/java/com/example/parayo/domain/auth/AuthRepository.kt
+++ app/src/main/java/com/example/parayo/domain/auth/AuthRepository.kt
@@ -4,29 +4,40 @@ import com.example.parayo.auth.model.AuthModel
 import com.example.parayo.auth.model.AuthRequest
 import com.example.parayo.auth.model.mapToPresenter
 import com.example.parayo.util.Result
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AuthRepository @Inject constructor(private val authService: AuthService) : AuthDataSource {
 
-    override suspend fun doSignUp(authRequest: AuthRequest): Result<AuthModel> = try {
-        val result = authService.signUp(authRequest)
-        if (result.success) {
-            Result.Success(AuthModel())
-        } else {
-            Result.Failure(IllegalStateException(result.message))
-        }
-    } catch (e: Exception) {
-        Result.Failure(e)
-    }
+    private val ioDispatcher = Dispatchers.IO
 
-    override suspend fun doSignIn(authRequest: AuthRequest): Result<AuthModel> = try {
-        val result = authService.signIn(authRequest)
-        if (result.success) {
-            Result.Success(result.data.mapToPresenter() ?: AuthModel())
-        } else {
-            Result.Failure(IllegalStateException(result.message))
+    override suspend fun doSignUp(authRequest: AuthRequest): Result<AuthModel> =
+        withContext(ioDispatcher) {
+            return@withContext try {
+                val result = authService.signUp(authRequest)
+                if (result.success) {
+                    Result.Success(AuthModel())
+                } else {
+                    Result.Failure(IllegalStateException(result.message))
+                }
+            } catch (e: Exception) {
+                Result.Failure(e)
+            }
         }
-    } catch (e: Exception) {
-        Result.Failure(e)
-    }
+
+    override suspend fun doSignIn(authRequest: AuthRequest): Result<AuthModel> =
+        withContext(ioDispatcher) {
+            return@withContext try {
+                println("${Thread.currentThread().name}")
+                val result = authService.signIn(authRequest)
+                if (result.success) {
+                    Result.Success(result.data.mapToPresenter() ?: AuthModel())
+                } else {
+                    Result.Failure(IllegalStateException(result.message))
+                }
+            } catch (e: Exception) {
+                Result.Failure(e)
+            }
+        }
 }
--- app/src/main/java/com/example/parayo/domain/product/ProductRepository.kt
+++ app/src/main/java/com/example/parayo/domain/product/ProductRepository.kt
@@ -2,6 +2,8 @@ package com.example.parayo.domain.product
 
 import android.net.Uri
 import com.example.parayo.util.Result
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -11,16 +13,21 @@ import javax.inject.Inject
 class ProductRepository @Inject constructor(private val productService: ProductService) :
     ProductDataSource {
 
-    override suspend fun uploadProductImage(imageUri: Uri): Result<Boolean> = try {
-        val result = productService.uploadProductImages(makeImagePart(imageUri))
-        if (result.success) {
-            Result.Success(true)
-        } else {
-            Result.Failure(IllegalStateException(result.message))
+    private val ioDispatcher = Dispatchers.IO
+
+    override suspend fun uploadProductImage(imageUri: Uri): Result<Boolean> =
+        withContext(ioDispatcher) {
+            return@withContext try {
+                val result = productService.uploadProductImages(makeImagePart(imageUri))
+                if (result.success) {
+                    Result.Success(true)
+                } else {
+                    Result.Failure(IllegalStateException(result.message))
+                }
+            } catch (e: Exception) {
+                Result.Failure(e)
+            }
         }
-    } catch (e: Exception) {
-        Result.Failure(e)
-    }
 
     private fun makeImagePart(imageUri: Uri): MultipartBody.Part {
         val (mediaType, imageFile) = MediaType.parse("multipart/form-data") to File(imageUri.toString())

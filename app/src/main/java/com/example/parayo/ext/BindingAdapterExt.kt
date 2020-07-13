6e95e5dea2dbfe8e175ab4b5df09c1da0306327b
BeokBeok <kekemusa37@nate.com>
2020-07-13 22:56:50 +0900

    [적용] 필드값이 없는 경우 예외 처리

--- app/src/main/java/com/example/parayo/ext/BindingAdapterExt.kt
+++ app/src/main/java/com/example/parayo/ext/BindingAdapterExt.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import android.widget.ImageView
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.BindingAdapter
+import com.example.parayo.R
 import com.example.parayo.wrapper.BeokImagePicker
 import com.google.android.material.textfield.TextInputLayout
 
@@ -16,10 +17,12 @@ fun showImagePicker(imageView: ImageView, index: Int, imgUpload: (index: Int, ur
 @BindingAdapter("bind:textInputLayoutErrorText")
 fun textInputLayoutErrorText(textInputLayout: TextInputLayout, errMsg: String) {
     textInputLayout.editText?.addTextChangedListener {
-        textInputLayout.error = if (it.toString().length > textInputLayout.counterMaxLength) {
-            errMsg
-        } else {
-            null
+        textInputLayout.error = when {
+            it.toString().length > textInputLayout.counterMaxLength -> errMsg
+            it.toString().isEmpty() -> {
+                textInputLayout.context.getString(R.string.msg_err_required_value)
+            }
+            else -> null
         }
     }
 }

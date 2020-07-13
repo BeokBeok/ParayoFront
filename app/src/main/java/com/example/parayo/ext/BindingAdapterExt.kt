0b606641dd60ae2f0ecc2063cebb1ffa0f7a8007
BeokBeok <kekemusa37@nate.com>
2020-07-13 22:25:00 +0900

    [추가] 상품명 입력 필드

--- app/src/main/java/com/example/parayo/ext/BindingAdapterExt.kt
+++ app/src/main/java/com/example/parayo/ext/BindingAdapterExt.kt
@@ -2,11 +2,24 @@ package com.example.parayo.ext
 
 import android.net.Uri
 import android.widget.ImageView
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.BindingAdapter
 import com.example.parayo.wrapper.BeokImagePicker
+import com.google.android.material.textfield.TextInputLayout
 
 @BindingAdapter("bind:index", "bind:showImagePicker", requireAll = true)
 fun showImagePicker(imageView: ImageView, index: Int, imgUpload: (index: Int, uri: Uri) -> Unit) =
     imageView.setOnClickListener {
         BeokImagePicker.showSingleImagePicker(imageView, index, imgUpload)
     }
+
+@BindingAdapter("bind:textInputLayoutErrorText")
+fun textInputLayoutErrorText(textInputLayout: TextInputLayout, errMsg: String) {
+    textInputLayout.editText?.addTextChangedListener {
+        textInputLayout.error = if (it.toString().length > textInputLayout.counterMaxLength) {
+            errMsg
+        } else {
+            null
+        }
+    }
+}
--- app/src/main/java/com/example/parayo/product/registration/ProductRegistrationViewModel.kt
+++ app/src/main/java/com/example/parayo/product/registration/ProductRegistrationViewModel.kt
@@ -1,12 +1,18 @@
 package com.example.parayo.product.registration
 
 import android.net.Uri
+import androidx.core.net.toUri
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.parayo.base.BaseViewModel
 import com.example.parayo.domain.product.ProductRepository
 import com.example.parayo.product.category.categoryList
+import com.example.parayo.util.Result
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class ProductRegistrationViewModel @ViewModelInject constructor(
     private val productRepository: ProductRepository
@@ -19,8 +25,7 @@ class ProductRegistrationViewModel @ViewModelInject constructor(
     private val _imageIds = MutableLiveData<List<Long>>()
     val imageIds: LiveData<List<Long>> get() = _imageIds
 
-    private val _productName = MutableLiveData<String>()
-    val productName: LiveData<String> get() = _productName
+    val productName = MutableLiveData<String>()
 
     private val _description = MutableLiveData<String>()
     val description: LiveData<String> get() = _description
@@ -35,25 +40,12 @@ class ProductRegistrationViewModel @ViewModelInject constructor(
     private var categoryIdSelected = categoryList[0].id
 
     private val descriptionLimit = 500
-    private val productNameLimit = 40
-
-    private val _productNameLength = MutableLiveData("0/$productNameLimit")
-    val productNameLength: LiveData<String> get() = _productNameLength
 
     private val _descriptionLength = MutableLiveData("0/$descriptionLimit")
     val descriptionLength: LiveData<String> get() = _descriptionLength
 
     val imgUpload = fun(index: Int, uri: Uri) {
         _imageUris[index].value = uri
-    }
-
-    fun checkProductNameLength() {
-        _productName.value?.let {
-            if (it.length > productNameLimit) {
-                _productName.value = it.take(productNameLimit)
-            }
-            _productNameLength.value = "${_productName.value?.length}/$productNameLimit"
-        }
     }
 
     fun checkDescriptionLength() {
@@ -67,5 +59,24 @@ class ProductRegistrationViewModel @ViewModelInject constructor(
 
     fun onCategorySelect(position: Int) {
         categoryIdSelected = categoryList[position].id
+    }
+
+    fun uploadProduct() = viewModelScope.launch {
+        val imageUriList = _imageUris.map { it.value ?: "".toUri() }
+        val requestList = mutableListOf<Deferred<Result<Boolean>>>()
+        for (uri in imageUriList) {
+            requestList.add(async { productRepository.uploadProductImage(uri) })
+        }
+
+        for (request in requestList) {
+            when (val result = request.await()) {
+                is Result.Success -> {
+                    if (!result.data) return@launch
+                }
+                is Result.Failure -> {
+                    return@launch
+                }
+            }
+        }
     }
 }
